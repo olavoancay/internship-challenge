f62df6a02fe533050d3e8d6e5ce901f94f88a417
olavoancay <olavoancay2802@gmail.com>
2024-05-24 18:59:34 -0300

    clarificando e corrigindo validação de entradas do usuário

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import { valida_entradas } from './valida_entradas';
 
 function App() {
     const [x, setX] = useState('');
@@ -10,20 +11,18 @@ function App() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setError('');
+        setError('');  //Adicionado para limpar os valores de resultado e erro quando se clica novamente
         setResult(null);
 
-        const xNum = parseInt(x);
-        const yNum = parseInt(y);
-
-        if (isNaN(xNum) || isNaN(yNum) || xNum <= 0 || yNum <= 0 || xNum >= yNum) {
-            setError('Por favor, insira inteiros positivos válidos onde x < y.');
+        const validationError = valida_entradas(x, y);
+        if (validationError) {
+            setError(validationError);
             return;
         }
 
         try {
             const response = await axios.get('http://localhost:8000/api/calcula-mmc-intervalo/', {
-                params: { x: xNum, y: yNum }
+                params: { x: parseInt(x), y: parseInt(y) }
             });
             setResult(response.data.result);
         } catch (err) {
@@ -32,36 +31,36 @@ function App() {
     };
 
     return (
-        <div className="App">
-            <div className="form-container">
-                <h1>Bem-vindo!</h1>
-                <p>
-                    Este formulário calcula o menor número divisível por todos os números contidos em um intervalo definido por um valor inicial e um valor final.
-                </p>
-                <p>
-                    <strong>Regras:</strong><br /><br />
-                   - Os 2 valores devem ser números inteiros positivos<br />
-                   - O valor inicial deve ser diferente do valor final<br />
-                   - O valor final deve ser maior do que o valor inicial
-                </p>
-                <div id='formulario'>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Valor Inicial:
-                        <input type="number" value={x} onChange={(e) => setX(e.target.value)} />
-                    </label>
-                    <label>
-                        Valor Final: 
-                        <input type="number" value={y} onChange={(e) => setY(e.target.value)} />
-                    </label>
-                    <button type="submit" >Calcular</button>
-                </form>
-                </div>
-                {error && <p className="error">{error}</p>}
-                {result && <p className="result">MMC: {result}</p>}
-            </div>
-        </div>
-    );
+      <div className="App">
+          <div className="form-container">
+              <h1>Bem-vindo!</h1>
+              <p>
+                  Este formulário calcula o menor número divisível por todos os números contidos em um intervalo definido por um valor inicial e um valor final.
+              </p>
+              <p>
+                  <strong>Regras:</strong><br /><br />
+                 - Os 2 valores devem ser números inteiros positivos<br />
+                 - O valor inicial deve ser diferente do valor final<br />
+                 - O valor final deve ser maior do que o valor inicial
+              </p>
+              <div id='formulario'>
+              <form onSubmit={handleSubmit}>
+                  <label>
+                      Valor Inicial:
+                      <input type="number" value={x} onChange={(e) => setX(e.target.value)} />
+                  </label>
+                  <label>
+                      Valor Final: 
+                      <input type="number" value={y} onChange={(e) => setY(e.target.value)} />
+                  </label>
+                  <button type="submit" >Calcular</button>
+              </form>
+              </div>
+              {error && <p className="error">{error}</p>}
+              {result && <p className="result">MMC: {result}</p>}
+          </div>
+      </div>
+  );
 }
 
 export default App;
