dbfefc9fbafd2ab0c918a246e666f3916248241d
olavoancay <olavoancay2802@gmail.com>
2024-05-24 18:19:16 -0300

    Estilizando e adicionando interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
         const yNum = parseInt(y);
 
         if (isNaN(xNum) || isNaN(yNum) || xNum <= 0 || yNum <= 0 || xNum >= yNum) {
-            setError('Please enter valid positive integers where x < y.');
+            setError('Por favor, insira inteiros positivos válidos onde x < y.');
             return;
         }
 
@@ -27,25 +27,39 @@ function App() {
             });
             setResult(response.data.result);
         } catch (err) {
-            setError('An error occurred while fetching the result.');
+            setError('Ocorreu um erro ao buscar o resultado.');
         }
     };
 
     return (
         <div className="App">
-            <form onSubmit={handleSubmit}>
-                <label>
-                    x:
-                    <input type="number" value={x} onChange={(e) => setX(e.target.value)} />
-                </label>
-                <label>
-                    y:
-                    <input type="number" value={y} onChange={(e) => setY(e.target.value)} />
-                </label>
-                <button type="submit">Calculate</button>
-            </form>
-            {error && <p className="error">{error}</p>}
-            {result && <p className="result">MMC: {result}</p>}
+            <div className="form-container">
+                <h1>Bem-vindo!</h1>
+                <p>
+                    Este formulário calcula o menor número divisível por todos os números contidos em um intervalo definido por um valor inicial e um valor final.
+                </p>
+                <p>
+                    <strong>Regras:</strong><br /><br />
+                   - Os 2 valores devem ser números inteiros positivos<br />
+                   - O valor inicial deve ser diferente do valor final<br />
+                   - O valor final deve ser maior do que o valor inicial
+                </p>
+                <div id='formulario'>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Valor Inicial:
+                        <input type="number" value={x} onChange={(e) => setX(e.target.value)} />
+                    </label>
+                    <label>
+                        Valor Final: 
+                        <input type="number" value={y} onChange={(e) => setY(e.target.value)} />
+                    </label>
+                    <button type="submit" >Calcular</button>
+                </form>
+                </div>
+                {error && <p className="error">{error}</p>}
+                {result && <p className="result">MMC: {result}</p>}
+            </div>
         </div>
     );
 }
